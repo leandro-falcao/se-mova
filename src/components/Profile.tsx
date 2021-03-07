9e5951a21629a1161c5b01df5e0fc1b19bd9af23
leandro falcao <leandrofalcaoti@gmail.com>
2021-03-07 09:04:08 -0300

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,19 +1,20 @@
 import styles from '../styles/components/Profile.module.css';
 
-export function Profile(){
-   return(
-      <div className={styles.profileContainer}>
-         <img src="https://github.com/leandro-falcao.png" 
-            alt="minha foto leandro" 
-         />
+import styles from '../styles/components/Profile.module.css';
 
-         <div>
-            <strong> leandro falcao</strong>
-            <p>
-               <img src="icons/level.svg" alt="icone level" />
-               leval 1</p>
-         </div>
-
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/leandro-falcao.png"
+        alt="Leandro Falcao"
+      />
+      <div>
+        <strong>Leandro Falcao </strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level
+        </p>
       </div>
-   );
+    </div>
+  );
 }
